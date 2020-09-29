7c531e66c9f985824ed737abe9e66055d0d9d4bc
F1 Mate <68943345+f1mate@users.noreply.github.com>
2020-09-29 18:58:24 +0530

    v1.00 20200929 1858
    Initial Commit.

---
+++ script.js
@@ -0,0 +1,22 @@
+/**
+ * File Name     : script.js
+ * Published by  : F1Mate
+ * Publisher URL : https://f1mate.com
+ * Contributors  : f1mate, 1amitgupta (Github)
+ * Version       : v1.00
+ * Github URL    : https://github.com/f1mate/blink-js-function
+ */
+
+/**
+ * 
+ * @param {String} ele_id <p>Element ID</p>
+ */
+function blinkJs(ele_id) {
+  let f = document.getElementById(ele_id);
+  if (f) {
+    setInterval(function() {
+      //f.style.display = (f.style.display == 'none' ? '' : 'none');
+      f.style.visibility = (f.style.visibility == 'hidden' ? '' : 'hidden');
+    }, 1000);
+  }
+}
